2fa84f30ac65ebfc577d8e07a784ecc3df3bd930
Topy <gmsdl1994@naver.com>
2022-09-02 04:28:29 +0900

    first commit

---
+++ src/main/java/study/jpastudy04/Jpastudy04Application.java
@@ -0,0 +1,13 @@
+package study.jpastudy04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpastudy04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jpastudy04Application.class, args);
+	}
+
+}
---
+++ src/test/java/study/jpastudy04/Jpastudy04ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jpastudy04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpastudy04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

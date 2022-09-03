30d296f4f7b30966c25ff68198b2d1e90c5b9a7b
Topy <gmsdl1994@naver.com>
2022-09-03 23:33:26 +0900

    Session 01 ~ 04

--- src/test/java/study/jpastudy04/Jpastudy04ApplicationTests.java
+++ src/test/java/study/jpastudy04/Jpastudy04ApplicationTests.java
@@ -1,13 +1,36 @@
 package study.jpastudy04;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.jpastudy04.entity.Hello;
+import study.jpastudy04.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @SpringBootTest
+@Transactional
 class Jpastudy04ApplicationTests {
+
+	@PersistenceContext
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
-	}
+		Hello hello = new Hello();
+		em.persist(hello);
 
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+	}
 }
